097d6cc6fc591ea6d1686e46753284c494af1b33
seth-willis <willis.rseth@gmail.com>
2020-05-13 14:19:58 -0700

    added javascript code

--- password_generator/script.js
+++ password_generator/script.js
@@ -1,6 +1,15 @@
 const passwordEL = document.getElementById('password')
 const generateEL = document.getElementById('generate')
+const sliderEL = document.getElementById('slider')
+const passwordlengthvalueEL = document.getElementById('passwordlengthvalue')
+const lowercaseEL = document.getElementById('lowercase')
+const uppercaseEL = document.getElementById('uppercase')
+const numbervarEL = document.getElementById('numbervar')
+const symbolvarEL = document.getElementById('symbolvar')
 
+//To-do Check for all checkboxes unchecked & alert
+
+passwordlengthvalueEL.innerHTML = sliderEL.value;
 
 const randomFunc = {
   lower: getRandomLower,
@@ -8,9 +17,27 @@ const randomFunc = {
   number: getRandomNumber,
   symbol: getRandomSymbol,
 }
+sliderEL.addEventListener('input', () =>{
+  passwordlengthvalueEL.innerHTML = sliderEL.value
+
+})
 
 generateEL.addEventListener('click', () =>{
-    password = getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol() +getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol() +getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol() + getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol() + getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol() +getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol() +getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol() + getRandomLower() + getRandomUpper() + getRandomNumber() + getRandomSymbol()
+    password = ''
+    for (x=0;x<sliderEL.value;x++) {
+      if(lowercaseEL.checked) {
+        password = password + getRandomLower()
+      }
+      if(uppercaseEL.checked) {
+        password = password + getRandomUpper()
+      }
+      if(numbervarEL.checked) {
+        password = password + getRandomNumber()
+      }
+      if(symbolvarEL.checked) {
+        password = password + getRandomSymbol()
+      }
+    }
     passwordEL.innerText = password
 })
 
